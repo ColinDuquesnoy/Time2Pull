6398c3852d5aa4f02413880bad36e2eb7b2055da
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-06-16 22:09:14 +0200

    Fix circular import

--- time2pull/__init__.py
+++ time2pull/__init__.py
@@ -18,20 +18,8 @@ Icons source:
 
 """
 
-__version__ = '1.0'
 
 
-import sys
-from PyQt5 import QtWidgets
-from time2pull.window import MainWindow
-from time2pull.settings import Settings
+__version__ = '1.0.2.1'
 
-
-def main():
-    global app, win
-    app = QtWidgets.QApplication(sys.argv)
-    win = MainWindow()
-    if not Settings().hide_on_startup:
-        win.show()
-    app.exec_()
 
